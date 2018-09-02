1a1f3b3c5895a54dc632b9234c036b77de81222a
erhunt29 <31340620+erhunt29@users.noreply.github.com>
2018-09-02 18:49:23 +0300

    Delete array.js

--- array.js
+++
@@ -1,4 +0,0 @@
-let carsString ='Toyota,Ford,Chevrolet,Nissan,Hyundai,KIA,Mercedes-Benz,BMW,Opel,Mazda,Acura,Volkswagen,Citroen,Volvo,Skoda,Land Rover,Renault,Honda,Datsun,Mitsubishi,Audi,Jeep,LADA,УАЗ,Peugeot,Daewoo,SsangYong,Lexus,Subaru,Suzuki,Infiniti,Fiat,Chery,Lifan,Brilliance,Geely,Great Wall';
-
-let cars = carsString.split(',').sort();
-
